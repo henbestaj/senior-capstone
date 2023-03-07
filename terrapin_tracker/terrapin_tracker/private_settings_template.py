5222f061c4a151a4905eb17f9b1e30eb91aa32a8
Aidan Henbest <henbestaj@gmail.com>
2023-03-07 12:17:09 -0500

    Connect MySQL server

--- terrapin_tracker/terrapin_tracker/private_settings_template.py
+++ terrapin_tracker/terrapin_tracker/private_settings_template.py
@@ -1,2 +1,3 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "insert secret key here and change this file's name to private_settings.py"
+SECRET_KEY = "insert Django secret key here and change this file's name to private_settings.py"
+password = "insert MySQL password here and change this file's name to private_settings.py"
--- terrapin_tracker/terrapin_tracker/settings.py
+++ terrapin_tracker/terrapin_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .private_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,8 +74,15 @@ WSGI_APPLICATION = "terrapin_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Turtles',
+        'USER': 'root',
+        'PASSWORD': password,
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -119,5 +127,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-from .private_settings import *
