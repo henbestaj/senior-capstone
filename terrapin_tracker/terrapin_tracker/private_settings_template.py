4bf8b3799ebc06e6b2e494f57cdd4a7e948efc01
Angelo-2231 <gangeloamato@gmail.com>
2023-03-10 09:00:58 -0500

    virtual environment

--- terrapin_tracker/terrapin_tracker/private_settings_template.py
+++
@@ -1,3 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "insert Django secret key here and change this file's name to private_settings.py"
-password = "insert MySQL password here and change this file's name to private_settings.py"
