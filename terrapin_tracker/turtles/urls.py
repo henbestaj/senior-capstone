dd4cabbccd36a62526d25195feb16b0cddc3b3d2
Aidan Henbest <henbestaj@gmail.com>
2023-03-30 12:08:28 -0400

    Make dynamic url for each turtle

--- terrapin_tracker/turtles/urls.py
+++ terrapin_tracker/turtles/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
   path("released/", released, name="released"),
   path("about/", about, name="about"),
   path("current/", current, name="current"),
+  path("current/<r_num>/<hatchling_num>", current_turtle, name="current_turtle"),
   path("signin/", signin, name="signin"),
   path("current/TurtleCreate/", TurtleCreate.as_view(), name="TurtleCreate"),
   path("current/MeasurementCreate/", MeasurementCreate.as_view(), name="MeasurementCreate"),
